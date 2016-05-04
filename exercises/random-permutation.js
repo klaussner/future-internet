f1672360c4952a72483681c4c0d3e9268c3212f9
Christian Klaussner <developer@klaussner.me>
2016-05-04 12:38:05 +0200

    Add command line parser

--- exercises/random-permutation.js
+++ exercises/random-permutation.js
@@ -2,3 +2,11 @@
 'use strict';
 
 const Switch = require('../shared/Switch.js');
+
+const argv = require('minimist')(process.argv.slice(2));
+
+if ('matrix' in argv) {
+  // TODO: Load traffic matrix
+}
+
+const s = new Switch(3);
